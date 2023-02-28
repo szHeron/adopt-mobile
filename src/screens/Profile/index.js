b834ba50bb15d3ab4be7e11e30171455857217b9
szheron <heronbr7@gmail.com>
2023-02-28 16:22:11 -0300

    feat: finalizado layout

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,9 +1,36 @@
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native';
+import { DefaultButton } from '../../components/DefaultButton';
+import { FontAwesome } from '@expo/vector-icons';
+import { styles } from './styles';
 
 export function Profile(){
     return (
-        <View>
-            <Text>Teste</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Perfil</Text>
+            <FontAwesome name='user-circle' size={156} color='#A5A5A5' />
+            <Text style={styles.textName}>Heron Rodrigues</Text>
+            <Text style={styles.textAccount}>
+                Conta
+            </Text>
+            <View style={styles.content}>
+                <View>
+                    <Text style={styles.textPlaceholder}>Nome</Text>
+                    <Text style={styles.textValue}>Heron Rodrigues de Oliveira</Text>
+                </View>
+                <View>
+                    <Text style={styles.textPlaceholder}>Email</Text>
+                    <Text style={styles.textValue}>Heronbr7@gmail.com</Text>
+                </View>
+                <View>
+                    <Text style={styles.textPlaceholder}>Telefone</Text>
+                    <Text style={styles.textValue}>(88)993721091</Text>
+                </View>
+            </View>
+            <DefaultButton style={{width: '50%'}}>
+                <Text>
+                    Editar
+                </Text>
+            </DefaultButton>
         </View>   
     )
 }
---
+++ src/screens/Profile/styles.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+    content: {
+        width: '90%',
+        backgroundColor: '#fff',
+        padding: 24,
+        borderRadius: 8,
+        marginBottom: 16
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: '600',
+        margin: 52
+    },
+    textName: {
+        fontSize: 18,
+        fontWeight: '500',
+        marginTop: 8
+    },
+    textAccount:{
+        color: '#A5A5A5',
+        fontSize: 20,
+        fontWeight: '600',
+        marginBottom: 4,
+        marginLeft: 18,
+        alignSelf: 'flex-start'
+    },
+    textPlaceholder: {
+        color: '#A5A5A5',
+        fontSize: 15,
+        fontWeight: '600',
+        marginBottom: 4
+    },
+    textValue: {
+        color: '#000',
+        fontSize: 15,
+        fontWeight: '700',
+        marginBottom: 16,
+        paddingBottom: 8,
+        borderBottomColor: '#A5A5A5',
+        borderBottomWidth: 1
+    }
+})
