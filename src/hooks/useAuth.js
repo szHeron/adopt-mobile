315f1a4a29e9e41fcfb1c2762b87e1d9e61b58d5
Heron <84138510+szHeron@users.noreply.github.com>
2023-04-02 14:09:45 -0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,8 @@
-import { useContext } from 'react';
+import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 export default function useAuth(){
-    const value = useContext(AuthContext);
+    const value = useContext(AuthContext)
     
-    return value;
+    return value
 }
